361cd1b2b5f2e9b4c16bbc2c77d05af310846035
Garry Brar <GarrySinghBr@gmail.com>
2025-11-01 17:36:55 -0400

    encode_decode

--- Arrays & Hashing/encode_decode.py
+++ Arrays & Hashing/encode_decode.py
@@ -11,35 +11,31 @@ Example 2:
 Input: ["we","say",":","yes"]
 Output: ["we","say",":","yes"]
 '''
-
 from typing import List
 
-START_OF_STRING = "#"
+DELIMITER = "#"
 
 def encode(strs: List[str]) -> str:
-    translation = []
-    for str in strs:
-        translation.append(START_OF_STRING)
-        for c in str:
-            translation.append(c)
-    return "".join(translation)
+    if len(strs) == 0:
+        return ""
+    scrambled = []
+    for word in strs:
+        length = len(word)
+        scrambled.append(str(length))
+        scrambled.append(DELIMITER)
+        for char in word:
+            scrambled.append(char)
+    return "".join(scrambled)
 
-# problem: delimiter only approach is messy, and will break on empty strings! RETHINK ENCODER!
-
-#we#say#:#yes
 def decode(s: str) -> List[str]:
-    original = []
-    word = []
-    for i in s:
-        if i != START_OF_STRING:
-            word.append(i)
-        else:
-            original.append("".join(word))
-            word.clear()
-    return original
-
-
-encoded = encode(["neet","code","love","you"])
-print(encoded)
-decoded = decode(encoded)
-print(decoded)
+    solution = []
+    i = 0
+    while i < len(s):
+        j = i
+        while s[j] != DELIMITER:
+            j = j +1
+        length = int(s[i:j])
+        word = s[j+1: j+length+1]
+        solution.append(word)
+        i = j + length + 1
+    return solution
